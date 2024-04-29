709582f54e8710261308e611eb955bd07b91827f
rbcribeiro <rbcribeiro@gmail.com>
2024-04-29 16:55:56 -0300

    "Estudando estruturas condicionais com Java"

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,15 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "G";
+
+        if(sigla == "P")
+            System.out.println("Pequeno");
+        else if(sigla == "M")
+            System.out.println("Médio");
+        else if(sigla == "G")
+            System.out.println("Grande");
+        else
+        System.out.println("Indefinido");
+    }
+    
+}
